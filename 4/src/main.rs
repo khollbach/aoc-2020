6578c77a0ea7d07855234cabaf9f656dbf64f2b4
Kevan Hollbach <khollbach@gmail.com>
2020-12-05 20:41:58 -0500

    4-2

--- 4/src/main.rs
+++ 4/src/main.rs
@@ -8,7 +8,8 @@ use std::io::Read;
 fn main() {
     let passports = read_input(io::stdin()).unwrap();
 
-    println!("{}", count_valid_passports(&passports));
+    println!("{}", count_valid_passports(&passports, false));
+    println!("{}", count_valid_passports(&passports, true));
 }
 
 type Passport = HashMap<String, String>;
@@ -43,28 +44,61 @@ fn read_input<R: Read>(mut input: R) -> Result<Vec<Passport>, Box<dyn Error>> {
     Ok(passports)
 }
 
-fn is_valid(
-    passport: &Passport,
-    req_fields: &HashSet<String>,
-    opt_fields: &HashSet<String>,
-) -> bool {
-    let all_fields_valid = passport
-        .keys()
-        .all(|k| req_fields.contains(k) || opt_fields.contains(k));
+fn is_valid<F>(passport: &Passport, req_fields: &HashSet<String>, is_valid_entry: F) -> bool
+where
+    F: Fn(&str, &str) -> bool,
+{
     let has_req_fields = req_fields.iter().all(|f| passport.contains_key(f));
+    let all_entries_valid = passport.iter().all(|(k, v)| is_valid_entry(k, v));
 
-    all_fields_valid && has_req_fields
+    has_req_fields && all_entries_valid
 }
 
-fn count_valid_passports(passports: &[Passport]) -> usize {
+fn is_valid_entry(key: &str, val: &str) -> bool {
+    let range = |v: &str, low, high| v.parse().map(|n| low <= n && n <= high).unwrap_or(false);
+    let range_4_digits = |v: &str, low, high| v.chars().count() == 4 && range(v, low, high);
+
+    match key {
+        "cid" => true,
+        "byr" => range_4_digits(val, 1920, 2002),
+        "iyr" => range_4_digits(val, 2010, 2020),
+        "eyr" => range_4_digits(val, 2020, 2030),
+        "hgt" => {
+            if let Some(caps) = Regex::new(r"^(\d+)cm$").unwrap().captures(val) {
+                range(&caps[1], 150, 193)
+            } else if let Some(caps) = Regex::new(r"^(\d+)in$").unwrap().captures(val) {
+                range(&caps[1], 59, 76)
+            } else {
+                false
+            }
+        }
+        "hcl" => Regex::new(r"^#[0-9a-f]{6}$").unwrap().is_match(val),
+        "ecl" => match val {
+            "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => true,
+            _ => false,
+        },
+        "pid" => Regex::new(r"^[0-9]{9}$").unwrap().is_match(val),
+        _ => false,
+    }
+}
+
+fn count_valid_passports(passports: &[Passport], strict_validation: bool) -> usize {
     let req_fields: HashSet<_> = vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
         .into_iter()
         .map(String::from)
         .collect();
-    let opt_fields: HashSet<_> = vec!["cid"].into_iter().map(String::from).collect();
+
+    // Toggle strict validation of entries (part 1 vs 2).
+    let is_valid_fn = |k: &str, v: &str| {
+        if strict_validation {
+            is_valid_entry(k, v)
+        } else {
+            k == "cid" || req_fields.contains(k)
+        }
+    };
 
     passports
         .iter()
-        .filter(|&p| is_valid(p, &req_fields, &opt_fields))
+        .filter(|&p| is_valid(p, &req_fields, is_valid_fn))
         .count()
 }
